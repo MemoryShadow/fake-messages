1f0d7abff07f154a17573a5c87b881008a91410e
MemoryShadow <JSker9@loft.games>
2024-10-21 05:52:17 +0000

    restore(sms): 将 API 路由迁回旧版本避免影响已有项目

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,11 +6,11 @@ router.get('/', function(_req, res) {
 })
 
 router.get('/emails/tags', require('./tags')('Email'))
-router.get('/short_messages/tags', require('./tags')('ShortMessage'))
+router.get('/messages/tags', require('./tags')('ShortMessage'))
 router.get('/templates/tags', require('./tags')('Template'))
 
 router.use('/emails', require('./emails'))
-router.use('/short_messages', require('./short_messages'))
+router.use('/messages', require('./short_messages'))
 router.use('/templates', require('./templates'))
 router.use('/private_numbers/bindings', require('./private_number_bindings'))
 router.use('/private_numbers/calls', require('./private_number_calls'))
